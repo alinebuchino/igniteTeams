ad8c0eebd9751628b16886f01e17016e8ebf4f36
Aline Buchino <aline.buchino@hotmail.com>
2023-07-21 22:54:56 -0300

    criando componente GroupCard e definindo suas propriedades

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,12 +1,15 @@
 import { Header } from "@components/Header";
 import { Container } from "./styles";
 import { SubHeader } from "@components/SubHeader";
+import { GroupCard } from "@components/GroupCard";
 
 export function Groups() {
   return (
     <Container>
       <Header />
       <SubHeader title="Turmas" subtitle="Jogue com a sua turma" />
+
+      <GroupCard title="Galera do Ignite" />
     </Container>
   );
 }
