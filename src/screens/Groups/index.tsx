6253acf1fbac6df01dca721a411ff544495a6013
Aline Buchino <aline.buchino@hotmail.com>
2023-07-21 22:25:11 -0300

    criando componente SubHeader e definindo suas propriedades

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,10 +1,12 @@
 import { Header } from "@components/Header";
 import { Container } from "./styles";
+import { SubHeader } from "@components/SubHeader";
 
 export function Groups() {
   return (
     <Container>
       <Header />
+      <SubHeader title="Turmas" subtitle="Jogue com a sua turma" />
     </Container>
   );
 }
