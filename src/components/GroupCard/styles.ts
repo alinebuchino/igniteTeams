02681a3e230ff242f1ad44accc9a6f868a67494b
Aline Buchino <aline.buchino@hotmail.com>
2023-07-22 21:32:19 -0300

    criando listagem dos grupos

--- src/components/GroupCard/styles.ts
+++ src/components/GroupCard/styles.ts
@@ -18,7 +18,7 @@ export const Container = styled(TouchableOpacity)`
 
 export const Title = styled.Text`
   color: ${({ theme }) => theme.COLORS.GRAY_200};
-  font-size: ${({ theme }) => theme.FONT_SIZE.MD};
+  font-size: ${({ theme }) => theme.FONT_SIZE.MD}px;
   font-family: ${({ theme }) => theme.FONT_FAMILY.REGULAR};
 `;
 
--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -2,14 +2,22 @@ import { Header } from "@components/Header";
 import { Container } from "./styles";
 import { SubHeader } from "@components/SubHeader";
 import { GroupCard } from "@components/GroupCard";
+import { useState } from "react";
+import { FlatList } from "react-native";
 
 export function Groups() {
+  const [groups, setGroups] = useState(["Galera da Rocket"]);
+
   return (
     <Container>
       <Header />
       <SubHeader title="Turmas" subtitle="Jogue com a sua turma" />
 
-      <GroupCard title="Galera do Ignite" />
+      <FlatList
+        data={groups}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => <GroupCard title={item} />}
+      />
     </Container>
   );
 }
