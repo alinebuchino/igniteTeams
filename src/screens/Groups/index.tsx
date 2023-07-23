75b1806f15b685fecd2a1b8dcd68d32a58d7799e
Aline Buchino <aline.buchino@hotmail.com>
2023-07-22 21:50:26 -0300

    componente ListEmpty

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -2,11 +2,12 @@ import { Header } from "@components/Header";
 import { Container } from "./styles";
 import { SubHeader } from "@components/SubHeader";
 import { GroupCard } from "@components/GroupCard";
+import { ListEmpty } from "@components/ListEmpty";
 import { useState } from "react";
 import { FlatList } from "react-native";
 
 export function Groups() {
-  const [groups, setGroups] = useState(["Galera da Rocket"]);
+  const [groups, setGroups] = useState([]);
 
   return (
     <Container>
@@ -17,6 +18,10 @@ export function Groups() {
         data={groups}
         keyExtractor={(item) => item}
         renderItem={({ item }) => <GroupCard title={item} />}
+        contentContainerStyle={groups.length === 0 && { flex: 1 }} // para deixar a mensagem centralizada
+        ListEmptyComponent={() => (
+          <ListEmpty message="Que tal cadastrar a primeira turma?" />
+        )}
       />
     </Container>
   );
