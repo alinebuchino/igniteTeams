a7390992c4efb17914a86c5c7e963d119859d0f0
Aline Buchino <aline.buchino@hotmail.com>
2023-08-15 23:11:05 -0300

    passando parametros pela rota

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -4,12 +4,15 @@ import { SubHeader } from "@components/SubHeader";
 import { Button } from "@components/Button";
 import { Input } from "@components/Input";
 import { useNavigation } from "@react-navigation/native";
+import { useState } from "react";
 
 export function NewGroup() {
+  const [group, setGroup] = useState("");
+
   const navigation = useNavigation();
 
   function handleNew() {
-    navigation.navigate("players", { group: "rocket" });
+    navigation.navigate("players", { group });
   }
   return (
     <Container>
@@ -20,7 +23,7 @@ export function NewGroup() {
           title="Nova turma"
           subtitle="Crie a tuma para adicionar as pessoas"
         />
-        <Input placeholder="Nome da turma" />
+        <Input placeholder="Nome da turma" onChangeText={setGroup} />
         <Button title="Criar" style={{ marginTop: 20 }} onPress={handleNew} />
       </Content>
     </Container>
--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,26 +1,32 @@
-import { Header } from "@components/Header";
+import { useState } from "react";
+import { FlatList } from "react-native";
+import { useRoute } from "@react-navigation/native";
+
 import { Container, Form, HeaderList, NumbersOfPlayers } from "./styles";
+
+import { Header } from "@components/Header";
 import { SubHeader } from "@components/SubHeader";
 import { ButtonIcon } from "@components/ButtonIcon";
 import { Input } from "@components/Input";
 import { Filter } from "@components/Filter";
-import { FlatList } from "react-native";
-import { useState } from "react";
 import { PlayersCard } from "@components/PlayersCard";
 import { ListEmpty } from "@components/ListEmpty";
 import { Button } from "@components/Button";
 
+type RouteParams = {
+  group: string;
+};
+
 export function Players() {
   const [team, setTeam] = useState("Time A");
   const [playes, setPlayes] = useState([]);
+  const route = useRoute();
+  const { group } = route.params as RouteParams;
 
   return (
     <Container>
       <Header showBackButton />
-      <SubHeader
-        title="Nome da turma"
-        subtitle="Adicione a galera e separe os times"
-      />
+      <SubHeader title={group} subtitle="Adicione a galera e separe os times" />
 
       <Form>
         <Input placeholder="Nome da pessoa" autoCorrect={false} />
