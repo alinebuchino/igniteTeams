02251d348b12c2cad9bf7bcae908dbba9494ef9c
Aline Buchino <aline.buchino@hotmail.com>
2023-08-16 23:04:15 -0300

    capturando todos os groups pra salvar em um array no storage

--- src/storage/group/groupCreate.ts
+++ src/storage/group/groupCreate.ts
@@ -1,9 +1,13 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { GROUP_COLLECTION } from "@storage/StorageConfig";
+import { groupsGetAll } from "./groupsGetAll";
 
 export async function groupCreate(newGroup: string) {
   try {
-    await AsyncStorage.setItem(GROUP_COLLECTION, newGroup);
+    const storageGroups = await groupsGetAll();
+    const storageConvert = JSON.stringify([...storageGroups, newGroup]);
+
+    await AsyncStorage.setItem(GROUP_COLLECTION, storageConvert);
   } catch (error) {
     throw error;
   }
